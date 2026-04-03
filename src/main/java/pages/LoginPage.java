c2adcb2cfe630d3333ccc20942a2191171ec05d2
DaricaS <daricastamkova11@gmail.com>
2026-04-03 20:29:14 +0200

    .

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -10,26 +10,39 @@ public class LoginPage {
     private By usernameField = By.id("username");
     private By passwordField = By.id("password");
     private By loginButton = By.cssSelector("button[type='submit']");
-    private By successMessage = By.id("flash");
-    private By errorMessage = By.id("flash");
+    private By message = By.id("flash");
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public void login(String username, String password) {
+    public void open() {
         driver.get("https://the-internet.herokuapp.com/login");
+    }
+
+    public void enterUsername(String username) {
+        driver.findElement(usernameField).clear();
         driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).clear();
         driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLogin() {
         driver.findElement(loginButton).click();
     }
 
-    public String getSuccessMessage() {
-        return driver.findElement(successMessage).getText();
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
     }
 
-    public String getErrorMessage() {
-        return driver.findElement(errorMessage).getText();
+    public String getMessage() {
+        return driver.findElement(message).getText();
     }
 }
+
 
