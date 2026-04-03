3df0dd2d35d59869907f263a812cf3c88197fce8
DaricaS <daricastamkova11@gmail.com>
2026-04-03 22:44:21 +0200

    .

--- src/main/java/listeners/TestListener.java
+++ src/main/java/listeners/TestListener.java
@@ -8,9 +8,25 @@ import utils.DriverManager;
 public class TestListener implements ITestListener {
 
     @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("Test Started: " + result.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("Test Passed: " + result.getName());
+    }
+
+    @Override
     public void onTestFailure(ITestResult result) {
+        System.out.println("Test Failed: " + result.getName());
         ScreenshotUtil.takeScreenshot(DriverManager.getDriver(), result.getName());
     }
 
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("Test Skipped: " + result.getName());
+    }
 }
+
 
