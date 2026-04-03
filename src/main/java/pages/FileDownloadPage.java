4be7dfbf7937a6d86a63c5e6c810a563a28b797d
DaricaS <daricastamkova11@gmail.com>
2026-04-03 19:33:45 +0200

    .

--- src/main/java/pages/FileDownloadPage.java
+++ src/main/java/pages/FileDownloadPage.java
@@ -3,25 +3,18 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-import java.io.File;
-
 public class FileDownloadPage {
 
     private WebDriver driver;
+
+    private By downloadLink = By.linkText("some-file.txt");
 
     public FileDownloadPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public void clickDownloadLink(String fileName) {
+    public void downloadFile() {
         driver.get("https://the-internet.herokuapp.com/download");
-        driver.findElement(By.linkText(fileName)).click();
-    }
-
-    public boolean verifyFileDownloaded(String fileName) {
-        String downloadPath = System.getProperty("user.dir") + "/downloads/";
-        File file = new File(downloadPath + fileName);
-        return file.exists();
+        driver.findElement(downloadLink).click();
     }
 }
-
--- src/main/java/pages/FileUploadPage.java
+++ src/main/java/pages/FileUploadPage.java
@@ -3,30 +3,33 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.io.File;
+
 public class FileUploadPage {
 
     private WebDriver driver;
 
     private By uploadInput = By.id("file-upload");
     private By uploadButton = By.id("file-submit");
-    private By uploadedFileName = By.id("uploaded-files");
+    private By uploadedFile = By.id("uploaded-files");
 
     public FileUploadPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public void uploadFile(String filePath) {
+    public void uploadFile(String fileName) {
         driver.get("https://the-internet.herokuapp.com/upload");
-        driver.findElement(uploadInput).sendKeys(filePath);
-    }
 
-    public void clickUploadButton() {
+        File file = new File(fileName);
+        driver.findElement(uploadInput).sendKeys(file.getAbsolutePath());
+
         driver.findElement(uploadButton).click();
     }
 
     public String getUploadedFileName() {
-        return driver.findElement(uploadedFileName).getText();
+        return driver.findElement(uploadedFile).getText();
     }
 }
+
 
 
