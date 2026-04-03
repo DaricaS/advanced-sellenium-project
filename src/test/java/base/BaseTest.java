b55e919d6ce0a340d1525b666afd25f7d9906867
DaricaS <daricastamkova11@gmail.com>
2026-04-03 20:50:06 +0200

    .

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,22 +1,30 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.DriverManager;
+import utils.ScreenshotUtil;
 
 public class BaseTest {
 
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
+    public void setup() {
         driver = DriverManager.getDriver();
     }
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+
+        if (ITestResult.FAILURE == result.getStatus()) {
+            ScreenshotUtil.takeScreenshot(driver, result.getName());
+        }
+
         DriverManager.quitDriver();
     }
 }
+
 
