f4b2d8b52c0f68fc119b2e5c0bb3d6ec3645e213
DaricaS <daricastamkova11@gmail.com>
2026-04-04 00:19:25 +0200

    .

--- src/main/java/listeners/TestListener.java
+++ src/main/java/listeners/TestListener.java
@@ -1,31 +1,43 @@
 package listeners;
 
+import utils.DriverManager;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import utils.ExtentManager;
 import utils.ScreenshotUtil;
-import utils.DriverManager;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 
 public class TestListener implements ITestListener {
 
+    private static ExtentReports extent = ExtentManager.getInstance();
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onStart(ITestContext context) {
+    }
+
     @Override
     public void onTestStart(ITestResult result) {
-        System.out.println("Test Started: " + result.getName());
+        test.set(extent.createTest(result.getName()));
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
-        System.out.println("Test Passed: " + result.getName());
+        test.get().pass("Test passed");
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-        System.out.println("Test Failed: " + result.getName());
+        test.get().fail(result.getThrowable());
         ScreenshotUtil.takeScreenshot(DriverManager.getDriver(), result.getName());
     }
 
     @Override
-    public void onTestSkipped(ITestResult result) {
-        System.out.println("Test Skipped: " + result.getName());
+    public void onFinish(ITestContext context) {
+        extent.flush();
     }
 }
 
